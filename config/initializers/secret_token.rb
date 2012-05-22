ca06708c7058dd3e2b8f420dc96e9eb4e073a0ef
B Kleinen <kleinen@infrastructure.de>
2012-05-23 00:43:54 +0200

    initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authlogic32::Application.config.secret_token = '5c4a0da39b5bc2869ffe8e0e6b3d44eba807e556e8da3413c4cddf1982f308aeee4f4951fcaf4cbf905ece7c807a77a84346c77807fdcd5be037305cd61d4f82'
